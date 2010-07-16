df33a20c1148a4b96d18880748088a4a272d2a1d
Alexander Konstantinov <aleksandrs.konstantinovs@itgrupa.lv>
2010-07-16 16:57:45 +0300

    Added solution to the problem #57

--- src/euler_057.hs
+++ src/euler_057.hs
@@ -1,10 +1,17 @@
--- Approximated square root of 2
-sqrt2 :: (Integral a, Floating b) => a -> b
-sqrt2 0 = 1.0
-sqrt2 n = 1 + 1 / (1 + sqrt2 (n - 1))
+-- Project Euler problem #57
 
-{-- Approximated square root of 2 (example of alternate declaration)
-sqrt2 n
-    | n == 0    = 1.0
-    | otherwise = 1 + 1 / (1 + sqrt2 (n - 1))
--}
+-- Calculates approximated square root of 2 as a fraction.
+-- Arguments:
+-- -- n: number of iterations
+-- Returns: tuple (numerator, denominator)
+sqrt2 0 = (1, 1)
+sqrt2 n = (a + 2 * b, a + b) where (a, b) = sqrt2 (n - 1)
+
+-- Returns True if fraction's numerator has more digits than its denominator
+hasLongerNumerator (a, b) = length (show a) > length (show b)
+
+-- Counts how much fractions from the list have numerator longer than denominator
+countLongerNumerators fs = sum [1 | f <- fs, hasLongerNumerator f]
+
+-- Solves the problem
+solveProblem = countLongerNumerators [sqrt2 n | n <- [1..1000]]
